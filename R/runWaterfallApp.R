0074f98bb1b86fbdf672ea1bb2a495a96647a197
Braisted <braistedjc@nih.gov>
2022-05-03 17:02:02 -0400

    updates for deployment

--- R/runWaterfallApp.R
+++ R/runWaterfallApp.R
@@ -13,3 +13,19 @@ runQHTSWaterfallApp <- function() {
   shiny::runApp(appDir, display.mode = "normal", test.mode = F, quiet = T, host="127.0.0.1")
 }
 
+
+#' Kicks off the shiny app
+#'
+#' @export
+runWaterfall <- function() {
+  print("in run waterfall")
+  print(getwd())
+  appDir <- system.file("./inst/shinyApp", package = 'qHTSWaterfall')
+  print("Hey set app dir... starting runApp")
+  #setwd("./inst/shinyApp/")
+  shiny::runApp(appDir, display.mode = "normal")
+  print("OK, App running")
+}
+
+
+
